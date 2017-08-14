2084679cd29bf415dfb9733055cc532b7a52780a
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-08-15 00:16:22 +0200

    innerHTML instead of textContent

--- js/w2c.js
+++ js/w2c.js
@@ -15,8 +15,8 @@
 
       if ( data.previews ) t.content.querySelector('img').src = data.previews[0];
 
-      t.content.querySelector('h3').textContent = data.title;
-      t.content.querySelector('p').textContent = data.abstract;
+      t.content.querySelector('h3').innerHTML = data.title;
+      t.content.querySelector('p').innerHTML = data.abstract;
 
       var clone = document.importNode(t.content, true);
       document.body.appendChild(clone);
