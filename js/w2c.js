e170239ef8d4bbec609832af1506e9d1bcede5a5
tkless <tmeskh2s@smail.inf.h-brs.de>
2017-11-14 16:17:25 +0100

    prevent multiple click events and modal dialogs bei clicking on same button

--- js/w2c.js
+++ js/w2c.js
@@ -50,40 +50,77 @@ $( document ).ready( function() {
     inner.find( '.detail' ).click( function () {
       renderComponentDetail( data );
     } );
-    inner.find( '.load-app-btn' ).attr(  'data-target', '#ccm-'+ data.name );
-    inner.find( '.save-as-new-btn' ).attr(  'data-target', '#ccm-'+ data.name +'-save-as-new' );
 
     if ( data.factories ) {
       inner.find( '.create' ).click( function () {
-        renderCreateComponent( data );
+        renderCreateAppView( data );
       });
     }
     else inner.find( '.create' ).addClass( 'disabled' );
 
     $( '#all' ).append( clone );
 
-    function renderCreateComponent( data ) {
+    function renderComponentDetail( data ) {
+      var clone = document.importNode( document.querySelector( '#component-detail' ).content, true );
+      var inner = $( clone.querySelector('div') );
 
-      // click event for rendering of load app modal dialog
-      inner.find( '.load-app-btn' ).click( function () {
-        var modal_clone = document.importNode( document.querySelector( '#load' ).content, true );
-        modal_clone.querySelector( '.modal' ).id = 'ccm-'+ data.name;
-        var modal_inner = $( modal_clone.querySelector('div') );
+      inner.find( 'img' ).attr( 'src', 'resources/component.png' );
+      inner.find( '#title' ).html( data.title );
+      inner.find( '.developer' ).html( data.developer + '<span class="glyphicon glyphicon-chevron-right"></span>' );
+      inner.find( '.lead' ).html = data.abstract;
+      inner.find( '#comp-name' ).html( data.name );
+      inner.find( '#developer' ).html( data.developer );
+      inner.find( '#website a' ).html( data.website );
+      inner.find( '#website a' ).attr( 'href', data.website ).attr( 'target', '_blank' );
+      inner.find( '#license' ).html( data.license );
 
-        // click Event of load-app button
-        modal_inner.find( '.load-app' ).click( function () {
-          if ( 'Web Component Cloud (W2C)' === modal_inner.find ( '#src option:selected' ).text() ) {
-            ccm.get( { store: 'w2c_' + data.name, url: 'https://ccm.inf.h-brs.de' }, modal_inner.find ('#key').val(), function ( result ) {
-              ccm.helper.encodeDependencies( result );
-              factory.config.start_values = result;
-              ccm.start( factory.url, factory.config, callback );
-            });
-          }
+      if (  data.description )
+        inner.find( '#description' ).html( data.description );
+      else {
+        inner.find( '.descr' ).remove();
+      }
+
+      var versions_elem = inner.find( '#versions' );
+      data.versions.map( function ( entry ) {
+        versions_elem.append( entry.version + ' - <a target="_blank" href="' + entry.source + '">source</a>' + ( entry.minified ? ' - <a target="_blank" href="' + entry.minified + '">minified</a>' : '' ) + '<br>' );
+      } );
+
+      if ( data.screenshots ) {
+        data.screenshots.map( function ( entry ) {
+          var screenshots_elem = document.importNode( document.querySelector( '#prev-img' ).content, true );
+          var inner_1 = $ ( screenshots_elem.querySelector( 'div' ) );
+          inner_1.find('img').attr( 'src', entry );
+          inner.find('#prev-thumbnail').append( inner_1 );
         });
+      } else {
+        inner.find( '#prev' ).remove();
+        inner.find( '.prev' ).remove();
+      }
 
-        document.body.appendChild( modal_clone );
-      });
+      if ( data.demos )
+        ccm.start( data.versions[0].minified ? data.versions[0].minified : data.versions[0].source, data.demos[0], function (instance) {
+          inner.find( '#demo-section' ).append( instance.root );
+        } );
+      else {
+        inner.find( '#demo' ).remove();
+        inner.find( '.demo' ).remove();
+      }
 
+      $( '#detail' ).html( '' );
+      $( '#detail' ).append( clone );
+
+    }
+
+    function renderCreateAppView( data ) {
+      var loaded_app_key;
+      var is_save_btn;
+
+      var embed_code_div_id;
+      var ccm_key_div_id;
+
+      initializeModalDilogs();
+
+      inner.find( '#storage' ).attr('value', '["ccm.store",{"store":"w2c_' + data.name + '","url":"https://ccm.inf.h-brs.de"}]');
 
       var config = data.factories[0].config;
 
@@ -92,14 +129,15 @@ $( document ).ready( function() {
         var store = { value: inner.find( '#storage' ).attr( 'value' ) };
         ccm.helper.decodeDependencies( store );
 
-        if ( inner.find( '#key' ).value )
-          comp_config.key = inner.find( '#key' ).value;
+        if ( loaded_app_key && is_save_btn ) {
+          comp_config.key = loaded_app_key;
+        }
 
         ccm.helper.solveDependency( store, 'value', function ( store ) {
           store.set( comp_config, function ( result ) {
             var embed_code = getEmbedCode( data.name, data.versions[0].version, { store: 'w2c_' + data.name, url: 'https://ccm.inf.h-brs.de' }, result.key );
-            $( '#embed-code' ).html( '<code>&lt;script src="'+ data.versions[0].source + '"&gt;&lt;/script&gt; '+ embed_code +'</code>' );
-            $( '#id' ).html('<pre>'+result.key+'</pre>');
+            $( embed_code_div_id ).html( '<code>&lt;script src="'+ data.versions[0].source + '"&gt;&lt;/script&gt; '+ embed_code +'</code>' );
+            $( ccm_key_div_id ).html('<pre>'+result.key+'</pre>');
             copyToClipboard();
           } );
         } );
@@ -110,24 +148,85 @@ $( document ).ready( function() {
       config.submit_button = false;
       config.preview = false;
 
-      var factory = data.factories[0];
-      ccm.start( factory.url, config, callback );
+      ccm.start( data.factories[0].url, config, onAfterFactoryStarted );
 
-      function callback( instance ) {
-        inner.find( '#storage' ).attr('value', '["ccm.store",{"store":"w2c_' + data.name + '","url":"https://ccm.inf.h-brs.de"}]');
+
+      function onAfterFactoryStarted( instance ) {
+
         inner.find( '#render-factory' ).html('');
-
-        // click event for rendering modal dialog  with app usage information
-        inner.find( '.save-as-new-btn' ).click( function () {
-          var modal_saved_clone = document.importNode( document.querySelector( '#save-as-new' ).content, true );
-          modal_saved_clone.querySelector( '.modal' ).id = 'ccm-'+ data.name +'-save-as-new';
-          document.body.appendChild(  modal_saved_clone );
-          instance.submit();
-        });
-
         inner.find( '#render-factory' ).append( instance.root );
         resizeHeight();
         renderPreview( instance, data );
+
+        inner.find( '.save-as-new-btn' ).click( function() {
+          embed_code_div_id = '#ccm-embed-code-'+ data.name+'-save-as';
+          ccm_key_div_id = '#ccm-id-'+ data.name+'-save-as';
+          is_save_btn = false;
+          instance.submit();
+        });
+        inner.find( '.save-btn' ).click( function() {
+          embed_code_div_id = '#ccm-embed-code-'+ data.name+'-save';
+          ccm_key_div_id = '#ccm-id-'+ data.name+'-save';
+          is_save_btn = true;
+          instance.submit();
+        });
+      }
+
+      function initializeModalDilogs() {
+        var factory = data.factories[0];
+
+        // click event for rendering of load app modal dialog
+        inner.find( '.load-app-btn' ).click( function () {
+
+          // set data-target attr. of modal dialog
+          inner.find( '.load-app-btn' ).attr(  'data-target', '#ccm-'+ data.name );
+
+          var load_app_clone = document.importNode( document.querySelector( '#load' ).content, true );
+          load_app_clone.querySelector( '.modal' ).id = 'ccm-'+ data.name;
+          var load_app_inner = $( load_app_clone.querySelector('div') );
+
+          // click Event of load-app button
+          load_app_inner.find( '.load-app' ).click( function () {
+            if ( 'Web Component Cloud (W2C)' === load_app_inner.find ( '#src option:selected' ).text() ) {
+              ccm.get( { store: 'w2c_' + data.name, url: 'https://ccm.inf.h-brs.de' }, load_app_inner.find ('#key').val(), function ( result ) {
+                loaded_app_key = load_app_inner.find ('#key').val();
+                ccm.helper.encodeDependencies( result );
+                factory.config.start_values = result;
+                ccm.start( factory.url, factory.config, onAfterFactoryStarted );
+
+                // display save-btn
+                $( '.save-btn' ).fadeIn( 2000 );
+              });
+            }
+          });
+          document.body.appendChild( load_app_clone );
+          $( this ).unbind( 'click' );
+        });
+
+        // click event for rendering modal dialog  with app usage information
+        inner.find( '.save-as-new-btn' ).click( function () {
+          inner.find( '.save-as-new-btn' ).attr(  'data-target', '#ccm-'+ data.name +'-save-as-new' );
+          var modal_save_as_clone = document.importNode( document.querySelector( '#save' ).content, true );
+          modal_save_as_clone.querySelector( '.modal' ).id = 'ccm-'+ data.name +'-save-as-new';
+          var modal_save_as_clone_inner = $( modal_save_as_clone.querySelector( 'div' ) );
+          modal_save_as_clone_inner.find('div span' ).eq(1).attr( 'id', 'ccm-embed-code-'+ data.name+'-save-as' );
+          modal_save_as_clone_inner.find( 'div span' ).eq(4).attr( 'id', 'ccm-id-'+ data.name+'-save-as' );
+          document.body.appendChild(  modal_save_as_clone );
+          $( this ).unbind( 'click' );
+        });
+
+        // click event for rendering of save-app modal dialog
+        inner.find( '.save-btn' ).click( function () {
+          inner.find( '.save-btn' ).attr( 'data-target', '#ccm-'+ data.name +'-save' );
+          var modal_saved_clone = document.importNode( document.querySelector( '#save' ).content, true );
+          modal_saved_clone.querySelector( '.modal' ).id = 'ccm-'+ data.name +'-save';
+          var modal_saved_clone_inner = $( modal_saved_clone.querySelector( 'div' ) );
+          modal_saved_clone_inner.find('div span' ).eq(1).attr( 'id', 'ccm-embed-code-'+ data.name+'-save' );
+          modal_saved_clone_inner.find( 'div span' ).eq(4).attr( 'id', 'ccm-id-'+ data.name+'-save' );
+          document.body.appendChild(  modal_saved_clone );
+          $( this ).unbind( 'click' );
+        });
+
       }
 
       function getEmbedCode( name, version, store_settings, key ) {
@@ -165,57 +264,6 @@ $( document ).ready( function() {
       }
 
     }
-  }
-
-  function renderComponentDetail( data ) {
-    var clone = document.importNode( document.querySelector( '#component-detail' ).content, true );
-    var inner = $( clone.querySelector('div') );
-
-    inner.find( 'img' ).attr( 'src', 'resources/component.png' );
-    inner.find( '#title' ).html( data.title );
-    inner.find( '.developer' ).html( data.developer + '<span class="glyphicon glyphicon-chevron-right"></span>' );
-    inner.find( '.lead' ).html = data.abstract;
-    inner.find( '#comp-name' ).html( data.name );
-    inner.find( '#developer' ).html( data.developer );
-    inner.find( '#website a' ).html( data.website );
-    inner.find( '#website a' ).attr( 'href', data.website ).attr( 'target', '_blank' );
-    inner.find( '#license' ).html( data.license );
-
-    if (  data.description )
-      inner.find( '#description' ).html( data.description );
-    else {
-      inner.find( '.descr' ).remove();
-    }
-
-    var versions_elem = inner.find( '#versions' );
-    data.versions.map( function ( entry ) {
-      versions_elem.append( entry.version + ' - <a target="_blank" href="' + entry.source + '">source</a>' + ( entry.minified ? ' - <a target="_blank" href="' + entry.minified + '">minified</a>' : '' ) + '<br>' );
-    } );
-
-    if ( data.screenshots ) {
-      data.screenshots.map( function ( entry ) {
-        var screenshots_elem = document.importNode( document.querySelector( '#prev-img' ).content, true );
-        var inner_1 = $ ( screenshots_elem.querySelector( 'div' ) );
-        inner_1.find('img').attr( 'src', entry );
-        inner.find('#prev-thumbnail').append( inner_1 );
-      });
-    } else {
-      inner.find( '#prev' ).remove();
-      inner.find( '.prev' ).remove();
-    }
-
-    if ( data.demos )
-      ccm.start( data.versions[0].minified ? data.versions[0].minified : data.versions[0].source, data.demos[0], function (instance) {
-        inner.find( '#demo-section' ).append( instance.root );
-      } );
-    else {
-      inner.find( '#demo' ).remove();
-      inner.find( '.demo' ).remove();
-    }
-
-    $( '#detail' ).html( '' );
-    $( '#detail' ).append( clone );
-
   }
 
   function resizeHeight() {
