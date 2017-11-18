b986c238349a309d779759ab875a5f8711c2a76c
Andre Kless <andre.kless@inf.h-brs.de>
2017-11-18 16:05:05 +0100

    add ccm.cloze_builder.js v1.2.0

--- resources/w2c_datasets.js
+++ resources/w2c_datasets.js
@@ -130,7 +130,7 @@ ccm.files[ "w2c_datasets.js" ] = {
     ],
     "factories": [
       {
-        "url": "https://akless.github.io/ccm-components/cloze_builder/versions/ccm.cloze_builder-1.1.0.min.js",
+        "url": "https://akless.github.io/ccm-components/cloze_builder/versions/ccm.cloze_builder-1.2.0.min.js",
         "config": {}
       },
       {
