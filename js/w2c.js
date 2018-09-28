6f5a4fc9969a9084ac8bac08866b6d098b8ad359
tkless <tmeskh2s@smail.inf.h-brs.de>
2018-09-28 12:14:22 +0200

    bugfix

--- js/w2c.js
+++ js/w2c.js
@@ -158,9 +158,10 @@ $( document ).ready( function() {
         inner.find( '#render-factory' ).append( instance.root );
         resizeHeight();
         renderPreview( instance, data );
-        instance.element.querySelector( '#button-advanced' ).addEventListener( 'click', function () {
-          resizeHeight();
-        } );
+        if ( instance.element.querySelector( '#button-advanced' ) )
+          instance.element.querySelector( '#button-advanced' ).addEventListener( 'click', function () {
+            resizeHeight();
+          } );
 
         inner.find( '.save-as-new-btn' ).click( function() {
           embed_code_div_id = '#ccm-embed-code-'+ data.key+'-save-as';
